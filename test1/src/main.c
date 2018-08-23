c04e561b937ea6a792d603065dfe3e80a188ea51
stephens2633 <stephens2633@gmail.com>
2018-08-22 23:12:30 -0400

    expanded some of the boiler plate

--- test1/src/main.c
+++ test1/src/main.c
@@ -3,155 +3,221 @@
 #include "stm32f4_discovery.h"
 
 #define GPIO_Pin_n GPIO_PIN_15
+#define GPIO_MODE             0x00000003U
+#define EXTI_MODE             0x10000000U
+#define GPIO_MODE_IT          0x00010000U
+#define GPIO_MODE_EVT         0x00020000U
+#define RISING_EDGE           0x00100000U
+#define FALLING_EDGE          0x00200000U
+#define GPIO_OUTPUT_TYPE      0x00000010U
+
+#define GPIO_NUMBER           16U
+
 
 int main(void) {
-	  RCC_OscInitTypeDef  RCC_OscInitStructval;
-	  RCC_OscInitTypeDef* RCC_OscInitStruct = &RCC_OscInitStructval;
-	  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef  RCC_OscInitStructval;
+	RCC_OscInitTypeDef* RCC_OscInitStruct = &RCC_OscInitStructval;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-	  __HAL_RCC_PWR_CLK_ENABLE();
+	__IO uint32_t tmpreg = 0x00U;
+	((RCC_TypeDef *) RCC_BASE)->APB1ENR |= RCC_APB1ENR_PWREN;
 
-	  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	/* Delay after an RCC peripheral clock enabling */
+	tmpreg = (((RCC_TypeDef *) RCC_BASE)->APB1ENR) & RCC_APB1ENR_PWREN;
 
-	  RCC_OscInitStructval.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	  RCC_OscInitStructval.HSEState = RCC_HSE_ON;
-	  RCC_OscInitStructval.PLL.PLLState = RCC_PLL_ON;
-	  RCC_OscInitStructval.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	  RCC_OscInitStructval.PLL.PLLM = 8;
-	  RCC_OscInitStructval.PLL.PLLN = 336;
-	  RCC_OscInitStructval.PLL.PLLP = RCC_PLLP_DIV2;
-	  RCC_OscInitStructval.PLL.PLLQ = 4;
 
-	   /* Check the parameters */
-	   assert_param(IS_RCC_OSCILLATORTYPE(RCC_OscInitStruct->OscillatorType));
-	   /*------------------------------- HSE Configuration ------------------------*/
-	   if(((RCC_OscInitStruct->OscillatorType) & RCC_OSCILLATORTYPE_HSE) == RCC_OSCILLATORTYPE_HSE)
+	tmpreg = 0x00U;
+
+	((PWR_TypeDef *) PWR_BASE)->CR &= (~(0x1U << (14U)) | (0x1U << (14U)));
+	/* Delay after an RCC peripheral clock enabling */
+	tmpreg = PWR->CR & PWR_CR_VOS;
+
+
+	RCC_OscInitStructval.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStructval.HSEState = RCC_HSE_ON;
+	RCC_OscInitStructval.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStructval.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStructval.PLL.PLLM = 8;
+	RCC_OscInitStructval.PLL.PLLN = 336;
+	RCC_OscInitStructval.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStructval.PLL.PLLQ = 4;
+
+	/*------------------------------- HSE Configuration ------------------------*/
+	if(((RCC_OscInitStruct->OscillatorType) & RCC_OSCILLATORTYPE_HSE) == RCC_OSCILLATORTYPE_HSE)
+	{
+
+	   /* Set the new HSE configuration ---------------------------------------*/
+	   __HAL_RCC_HSE_CONFIG(RCC_OscInitStruct->HSEState);
+
+	   /* Check the HSE State */
+	   if((RCC_OscInitStruct->HSEState) != RCC_HSE_OFF)
 	   {
-	     /* Check the parameters */
-	     assert_param(IS_RCC_HSE(RCC_OscInitStruct->HSEState));
-	       /* Set the new HSE configuration ---------------------------------------*/
-	       __HAL_RCC_HSE_CONFIG(RCC_OscInitStruct->HSEState);
-
-	       /* Check the HSE State */
-	       if((RCC_OscInitStruct->HSEState) != RCC_HSE_OFF)
-	       {
-	         /* Wait till HSE is ready */
-	         while(__HAL_RCC_GET_FLAG(RCC_FLAG_HSERDY) == RESET);
-	       }
+		 /* Wait till HSE is ready */
+		 while(__HAL_RCC_GET_FLAG(RCC_FLAG_HSERDY) == RESET);
 	   }
+	}
 
-	   /*-------------------------------- PLL Configuration -----------------------*/
-	   /* Check the parameters */
-	   assert_param(IS_RCC_PLL(RCC_OscInitStruct->PLL.PLLState));
-	   if ((RCC_OscInitStruct->PLL.PLLState) != RCC_PLL_NONE)
+	/*-------------------------------- PLL Configuration -----------------------*/
+
+	if ((RCC_OscInitStruct->PLL.PLLState) != RCC_PLL_NONE)
+	{
+	 /* Check if the PLL is used as system clock or not */
+	 if(__HAL_RCC_GET_SYSCLK_SOURCE() != RCC_CFGR_SWS_PLL)
+	 {
+	   if((RCC_OscInitStruct->PLL.PLLState) == RCC_PLL_ON)
 	   {
-	     /* Check if the PLL is used as system clock or not */
-	     if(__HAL_RCC_GET_SYSCLK_SOURCE() != RCC_CFGR_SWS_PLL)
-	     {
-	       if((RCC_OscInitStruct->PLL.PLLState) == RCC_PLL_ON)
-	       {
-	         /* Check the parameters */
-	         assert_param(IS_RCC_PLLSOURCE(RCC_OscInitStruct->PLL.PLLSource));
-	         assert_param(IS_RCC_PLLM_VALUE(RCC_OscInitStruct->PLL.PLLM));
-	         assert_param(IS_RCC_PLLN_VALUE(RCC_OscInitStruct->PLL.PLLN));
-	         assert_param(IS_RCC_PLLP_VALUE(RCC_OscInitStruct->PLL.PLLP));
-	         assert_param(IS_RCC_PLLQ_VALUE(RCC_OscInitStruct->PLL.PLLQ));
 
-	         /* Disable the main PLL. */
-	         __HAL_RCC_PLL_DISABLE();
 
-	         /* Wait till PLL is ready */
-	         while(__HAL_RCC_GET_FLAG(RCC_FLAG_PLLRDY) != RESET);
+		 /* Wait till PLL is ready */
+		 while(__HAL_RCC_GET_FLAG(RCC_FLAG_PLLRDY) != RESET);
 
-	         /* Configure the main PLL clock source, multiplication and division factors. */
-	         WRITE_REG(RCC->PLLCFGR, (RCC_OscInitStruct->PLL.PLLSource                                            | \
-	                                  RCC_OscInitStruct->PLL.PLLM                                                 | \
-	                                  (RCC_OscInitStruct->PLL.PLLN << POSITION_VAL(RCC_PLLCFGR_PLLN))             | \
-	                                  (((RCC_OscInitStruct->PLL.PLLP >> 1U) - 1U) << POSITION_VAL(RCC_PLLCFGR_PLLP)) | \
-	                                  (RCC_OscInitStruct->PLL.PLLQ << POSITION_VAL(RCC_PLLCFGR_PLLQ))));
-	         /* Enable the main PLL. */
-	         __HAL_RCC_PLL_ENABLE();
+		 /* Configure the main PLL clock source, multiplication and division factors. */
+		RCC->PLLCFGR= (RCC_OscInitStruct->PLL.PLLSource                                            | \
+								  RCC_OscInitStruct->PLL.PLLM                                                 | \
+								  (RCC_OscInitStruct->PLL.PLLN << POSITION_VAL(RCC_PLLCFGR_PLLN))             | \
+								  (((RCC_OscInitStruct->PLL.PLLP >> 1U) - 1U) << POSITION_VAL(RCC_PLLCFGR_PLLP)) | \
+								  (RCC_OscInitStruct->PLL.PLLQ << POSITION_VAL(RCC_PLLCFGR_PLLQ)));
 
-	         /* Wait till PLL is ready */
-	         while(__HAL_RCC_GET_FLAG(RCC_FLAG_PLLRDY) == RESET);
-	       }
-	     }
+		 /* Enable the main PLL. */
+		(*(__IO uint32_t *) RCC_CR_PLLON_BB = ENABLE);
+
+		 /* Wait till PLL is ready */
+		 while(__HAL_RCC_GET_FLAG(RCC_FLAG_PLLRDY) == RESET);
 	   }
-	  //----------------------------------
+	 }
+	}
+	//----------------------------------
 
-	  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-	                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
-	  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK);
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+							  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV4;
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK);
 
 
 
-	  //HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	//HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-	  uint32_t ticks = HAL_RCC_GetHCLKFreq()/1000;
-	  if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk)
+	uint32_t ticks = HAL_RCC_GetHCLKFreq()/1000;
+	if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk)
+	{
+	return (1UL);                                                   /* Reload value impossible */
+	}
+
+	SysTick->LOAD  = (uint32_t)(ticks - 1UL);                         /* set reload register */
+	NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL); /* set Priority for Systick Interrupt */
+	SysTick->VAL   = 0UL;                                             /* Load the SysTick Counter Value */
+	SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
+				   SysTick_CTRL_TICKINT_Msk   |
+				   SysTick_CTRL_ENABLE_Msk;                         /* Enable SysTick IRQ and SysTick Timer */
+
+
+	SysTick->CTRL |= SYSTICK_CLKSOURCE_HCLK;
+
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+
+
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	//__HAL_RCC_GPIOH_CLK_ENABLE();
+	tmpreg = 0x00U;
+	/* GPIO Ports Clock Enable */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
+	/* Delay after an RCC peripheral clock enabling */
+	tmpreg = RCC->AHB1ENR & RCC_AHB1ENR_GPIOHEN;
+
+	//__HAL_RCC_GPIOD_CLK_ENABLE();
+	tmpreg = 0x00U;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	/* Delay after an RCC peripheral clock enabling */
+	tmpreg = RCC->AHB1ENR & RCC_AHB1ENR_GPIODEN;
+
+
+	/*Configure GPIO pin Output Level */
+	GPIOD->BSRR = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+
+	/*Configure GPIO pins : PD12 PD13 PD14 PD15 */
+	GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+
+
+
+	GPIO_InitTypeDef * GPIO_Init = &GPIO_InitStruct;
+	GPIO_TypeDef* GPIOx = GPIOD;
+
+
+	uint32_t position;
+	uint32_t ioposition = 0x00U;
+	uint32_t iocurrent = 0x00U;
+	uint32_t temp = 0x00U;
+
+	/* Configure the port pins */
+	for(position = 0U; position < 16U; position++)
+	{
+	  /* Get the IO position */
+	  ioposition = 0x01U << position;
+	  /* Get the current IO position */
+	  iocurrent = (uint32_t)(GPIO_Init->Pin) & ioposition;
+
+	  if(iocurrent == ioposition)
 	  {
-	    return (1UL);                                                   /* Reload value impossible */
+		/*--------------------- GPIO Mode Configuration ------------------------*/
+		/* In case of Alternate function mode selection */
+		if((GPIO_Init->Mode == GPIO_MODE_AF_PP) || (GPIO_Init->Mode == GPIO_MODE_AF_OD))
+		{
+
+		  /* Configure Alternate function mapped with the current IO */
+		  temp = GPIOx->AFR[position >> 3U];
+		  temp &= ~(0xFU << ((uint32_t)(position & 0x07U) * 4U)) ;
+		  temp |= ((uint32_t)(GPIO_Init->Alternate) << (((uint32_t)position & 0x07U) * 4U));
+		  GPIOx->AFR[position >> 3U] = temp;
+		}
+
+		/* Configure IO Direction mode (Input, Output, Alternate or Analog) */
+		temp = GPIOx->MODER;
+		temp &= ~(GPIO_MODER_MODER0 << (position * 2U));
+		temp |= ((GPIO_Init->Mode & GPIO_MODE) << (position * 2U));
+		GPIOx->MODER = temp;
+
+		/* In case of Output or Alternate function mode selection */
+		if((GPIO_Init->Mode == GPIO_MODE_OUTPUT_PP) || (GPIO_Init->Mode == GPIO_MODE_AF_PP) ||
+		   (GPIO_Init->Mode == GPIO_MODE_OUTPUT_OD) || (GPIO_Init->Mode == GPIO_MODE_AF_OD))
+		{
+
+		  /* Configure the IO Speed */
+		  temp = GPIOx->OSPEEDR;
+		  temp &= ~(GPIO_OSPEEDER_OSPEEDR0 << (position * 2U));
+		  temp |= (GPIO_Init->Speed << (position * 2U));
+		  GPIOx->OSPEEDR = temp;
+
+		  /* Configure the IO Output Type */
+		  temp = GPIOx->OTYPER;
+		  temp &= ~(GPIO_OTYPER_OT_0 << position) ;
+		  temp |= (((GPIO_Init->Mode & GPIO_OUTPUT_TYPE) >> 4U) << position);
+		  GPIOx->OTYPER = temp;
+		}
+
+		/* Activate the Pull-up or Pull down resistor for the current IO */
+		temp = GPIOx->PUPDR;
+		temp &= ~(GPIO_PUPDR_PUPDR0 << (position * 2U));
+		temp |= ((GPIO_Init->Pull) << (position * 2U));
+		GPIOx->PUPDR = temp;
+
+
 	  }
-
-	  SysTick->LOAD  = (uint32_t)(ticks - 1UL);                         /* set reload register */
-	  NVIC_SetPriority (SysTick_IRQn, (1UL << __NVIC_PRIO_BITS) - 1UL); /* set Priority for Systick Interrupt */
-	  SysTick->VAL   = 0UL;                                             /* Load the SysTick Counter Value */
-	  SysTick->CTRL  = SysTick_CTRL_CLKSOURCE_Msk |
-	                   SysTick_CTRL_TICKINT_Msk   |
-	                   SysTick_CTRL_ENABLE_Msk;                         /* Enable SysTick IRQ and SysTick Timer */
+	}
 
 
-
-	  //HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-	  /* Check the parameters */
-	  assert_param(IS_SYSTICK_CLK_SOURCE(CLKSource));
-
-      SysTick->CTRL |= SYSTICK_CLKSOURCE_HCLK;
-
-	  /* SysTick_IRQn interrupt configuration */
-	  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-
-
-	  GPIO_InitTypeDef GPIO_InitStruct;
-
-	  //__HAL_RCC_GPIOH_CLK_ENABLE();
-	  __IO uint32_t tmpreg = 0x00U;
-	  /* GPIO Ports Clock Enable */
-	  do {
-
-		  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOHEN);
-		  /* Delay after an RCC peripheral clock enabling */
-		  tmpreg = READ_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOHEN);
-		  UNUSED(tmpreg);
-	   } while(0U);
-	  //__HAL_RCC_GPIOD_CLK_ENABLE();
-
-	  do {
-	  		  tmpreg = 0x00U;
-	  		  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIODEN);
-	  		  /* Delay after an RCC peripheral clock enabling */
-	  		  tmpreg = READ_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIODEN);
-	  		  UNUSED(tmpreg);
-	  	   } while(0U);
-
-
-	  /*Configure GPIO pin Output Level */
-	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_SET);
-
-	  /*Configure GPIO pins : PD12 PD13 PD14 PD15 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
-	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-	  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-
+	  //----------------------------------------------
 
     while (1) {
-    	HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
+    	  GPIOD->ODR ^= GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
     	  uint32_t tickstart = HAL_GetTick();
     	  uint32_t wait = 1000;
 
@@ -160,6 +226,7 @@ int main(void) {
     	  {
     	     wait++;
     	  }
+
 
     	  while((HAL_GetTick() - tickstart) < wait)
     	  {
